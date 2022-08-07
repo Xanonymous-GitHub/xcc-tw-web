ad336c8046d7dd1bdba503268a707335ff11cf64
Xanonymous <trusaidlin@gmail.com>
2022-08-08 04:30:27 +0800

    refactor: smaller button

--- components/Logos.vue
+++ components/Logos.vue
@@ -26,7 +26,7 @@ import parrot from '~/assets/images/parrot.png'
         So, for more about me, please visit my GitHub directly by clicking the button below!
       </h6>
 
-      <a href="https://s.xcc.tw" target="__blank" rel="nofollow noreferrer noopener" class="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
+      <a href="https://s.xcc.tw" my-4 target="__blank" rel="nofollow noreferrer noopener" class="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
         <span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100" />
         <!-- Top glass gradient -->
         <span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3" />
@@ -38,7 +38,7 @@ import parrot from '~/assets/images/parrot.png'
         <span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5" />
         <span class="absolute inset-0 w-full h-full border border-white rounded-md opacity-10" />
         <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5" />
-        <span class="relative">SXCCTW</span>
+        <span class="relative" text-sm>SXCCTW</span>
       </a>
     </div>
   </div>
