692968e8fede26b0a0045db958a4d497341db070
Xanonymous <trusaidlin@gmail.com>
2022-08-16 03:37:12 +0800

    feat: add head

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: 'XCCTW',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+})
+</script>
+
 <template>
   <Home />
 </template>
